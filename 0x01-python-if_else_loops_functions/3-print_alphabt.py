7f54ff67983c2187ca030c6a1edf3e37c5a6906e
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-09-21 12:55:15 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z")+1):
-    if 1 == ord("q") or i == ord("e"):
+    if i == ord("q") or i == ord("e"):
         continue
+
     print("{:c}".format(i), end="")
