07a83ce5251626a99faebfd602d93354500dfe71
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-09-25 13:11:40 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_interger(my_list=[]):
+def print_list_integer(my_list=[]):
     for m in my_list:
-        print("{:d}".format(m))
+        print('{:d}'.format(m))
--- 0x03-python-data_structures/list.h
+++
@@ -1,5 +0,0 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stdlib.h>
-
-def print_list_integer(my_list=[]):
