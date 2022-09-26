98ab8072cc259609b5f546e5e8af5cccd848ff34
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-09-26 14:02:28 -0700

    11-delete_at.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+
+    for m in my_list:
+        if m % 2 == 0:
+            new_list.append(true)
+        else:
+            new_list.append(false)
+    return (new_list)
