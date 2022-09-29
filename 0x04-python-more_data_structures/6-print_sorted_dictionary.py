04776cb139e4840721cc70d959829cca6330af46
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-09-28 19:42:10 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dict):
+    "print a dict in sorted order of keys"
+    if a_dictionary is None:
+        return None
+    for key in sorted(a_dictionary.keys()):
+        print("{:s}: {}".format(key, a_dictionary[keys]))
