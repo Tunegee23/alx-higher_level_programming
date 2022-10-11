2cfe1321b8caf898ec1dc98c56b769ae2a77eafe
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-10-11 14:27:02 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,14 +2,11 @@
 """Defines a square """
 
 
-class Square:
+class square:
     """Represents a square"""
-    pass
 
-
-def __init__(self, size):
-    """Initializing this square class
-    Args: size - represnets the size of the square defined
-    """
-
-    self.__size = size
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+        self.__size = size
